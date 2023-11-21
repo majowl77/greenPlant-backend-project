8d83b01e75496f616b5b28705c25cf60b636b2ee
ibtisamMDev <ebtissam.socialmedia@gmail.com>
2023-11-21 19:15:48 +0300

    Creating model for category

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,12 @@ import mongoose from 'mongoose'
 import { config } from 'dotenv'
 
 import { dev } from './config/index'
+//import router
 import usersRouter from './routers/users'
 import productsRouter from './routers/products'
 import ordersRouter from './routers/orders'
+import categoryRouter from './routers/categories'
+// import middlewares
 import apiErrorHandler from './middlewares/errorHandler'
 import myLogger from './middlewares/logger'
 
@@ -17,10 +20,11 @@ const URL = dev.app.db as string
 app.use(myLogger)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-
+//for testing endpoint
 app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
 app.use('/api/products', productsRouter)
+app.use('/api/categories',categoryRouter )
 
 app.use(apiErrorHandler)
 
