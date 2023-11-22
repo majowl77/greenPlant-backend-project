c84dfed066ca407e9f3adbe74e9da14318659cb8
Layan-Dev <layanalghamdibusiness@hotmail.com>
2023-11-22 14:00:46 +0300

    few changes on product model

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,8 @@
 import 'dotenv/config';
 
 export const dev={
-    app:{port:Number(process.env.PORT)|| 3003},
-    db:{
-        url:
-        process.env.MONGODB_URL||'mongodb:localhost:3003/backend_project'
-    },
+    app:{
+        port: process.env.PORT,
+    db: process.env.MONGODB_URL
+    } ,
 }
