710d84cecdf2f5891c80a809330312e402237188
Layan-Dev <layanalghamdibusiness@hotmail.com>
2023-11-22 09:29:18 +0300

    finishe products models and crouters

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose'
 
+export type ProductDocument = Document & {
+  name: string
+ 
+}
+
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -12,8 +17,37 @@ const productSchema = new mongoose.Schema({
   },
   quantity: {
     type: Number,
-    default: 1,
+    default:1
+  },
+
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: [{
+      type:String,
+      required:true,
+        }],
+   
+  },
+  variants: {
+    type: [{
+      type:String,
+    }],
+    
+  },
+  sizes: {
+    type: [{
+      type:String,
+    }],
   },
 })
 
+//const Product = mongoose.model('Product',productSchema );
 export default mongoose.model('Product', productSchema)
+
