dbe73e888146d585125db96317ac04199284a128
Majedah <122641068+majowl77@users.noreply.github.com>
2023-11-22 14:51:44 +0300

    fix: resolving conflict

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -14,8 +14,6 @@ export const getAllcategories =async (  req:Request ,
          } catch(error){
             next(error);
          }
-            
-       
 
     
 }}
--- src/routers/products.ts
+++ src/routers/products.ts
@@ -4,7 +4,13 @@ const router = express.Router()
 import Product from '../models/product'
 import Order from '../models/order'
 import ApiError from '../errors/ApiError'
-import { createNewProduct, deleteProductById, getAllProducts, getProductById, updateProductById } from '../controllers/productController'
+import {
+  createNewProduct,
+  deleteProductById,
+  getAllProducts,
+  getProductById,
+  updateProductById,
+} from '../controllers/productController'
 
 router.get('/', getAllProducts)
 
@@ -15,19 +21,17 @@ router.post('/', createNewProduct)
 router.put('/:productId', updateProductById)
 
 router.delete('/:productId', deleteProductById)
-//Implement a route to handle GET requests with query parameters for filtering items 
+//Implement a route to handle GET requests with query parameters for filtering items
 //or products based on specific criteria (e.g., by category, price range).
 
 //fiter by variants
-router.get('/variants/{variant}',async(req,res)=>{
-  const selectedVariant= req.body.variants
-  const productByVar= await Product.find({
-    _var:selectedVariant,
+router.get('/variants/{variant}', async (req, res) => {
+  const selectedVariant = req.body.variants
+  const productByVar = await Product.find({
+    _var: selectedVariant,
   })
   res.status(200).json(productByVar)
-
 })
 //fiter by size
-
 
 export default router
