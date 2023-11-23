2f7d7e0eb7e1fb07d53d4f584b935b0970219856
Majedah <122641068+majowl77@users.noreply.github.com>
2023-11-23 09:27:55 +0300

    feat: sort by order:asc or desc

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+
 import {
   registerNewUser,
   deleteUser,
@@ -8,18 +9,15 @@ import {
 } from '../controllers/userController'
 
 const router = express.Router()
-router.get('/', getUsers)
-router.post('/register', registerNewUser)
-router.post('/login', loginUser)
-router.delete('/:userId', deleteUser)
-router.put('/:userId', updateUser)
 
-// i will work on it in the bouns and additional req
-// router.get('/:userId/page/:page', (req, res) => {
-//   res.json({
-//     msg: 'done',
-//     user: req.user,
-//   })
-// })
+router.get('/', getUsers)
+
+router.post('/register', registerNewUser)
+
+router.post('/login', loginUser)
+
+router.delete('/:userId', deleteUser)
+
+router.put('/:userId', updateUser)
 
 export default router
