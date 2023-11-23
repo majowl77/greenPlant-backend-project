87d408aed3308b20d44f9672ac527dc39162aaa9
AbdulazizAlzahrani <abdulaziz.alzahrani.14@outlook.com>
2023-11-23 14:47:07 +0300

    comment the errors i could't solve in order

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -28,9 +28,9 @@ export const addNewOrder = async (req: Request, res: Response, next: NextFunctio
   try {
     const { products, userId } = req.body
 
-    for (let productId in products) {
-      reduceProductQtyByOne(productId, next)
-    }
+    // for (let productId in products) {
+    //   reduceProductQtyByOne(productId, next)
+    // }
 
     const order = new Order({
       products,
@@ -45,31 +45,31 @@ export const addNewOrder = async (req: Request, res: Response, next: NextFunctio
   }
 }
 
-export const reduceProductQtyByOne = async (productId: string, next: NextFunction) => {
-  try {
-    const product = await Product.findById(productId)
+// export const reduceProductQtyByOne = async (productId: string, next: NextFunction) => {
+//   try {
+//     const product = await Product.findById(productId)
 
-    if (!product) {
-      next(ApiError.notFound(`Product with ID ${productId} not found.`))
-      return
-    }
+//     if (!product) {
+//       next(ApiError.notFound(`Product with ID ${productId} not found.`))
+//       return
+//     }
 
-    if (product.quantity > 0) {
-      const updatedProduct = await Product.findOneAndUpdate(
-        { _id: productId },
-        { $inc: { quantity: -1 } },
-        { new: true }
-      )
+//     if (product.quantity > 0) {
+//       const updatedProduct = await Product.findOneAndUpdate(
+//         { _id: productId },
+//         { $inc: { quantity: -1 } },
+//         { new: true }
+//       )
 
-      // Respond with the updated product
-      // You might want to send a response to the client here
-    } else {
-      next(ApiError.unauthorized(`Product quantity for ${productId} is already zero.`))
-    }
-  } catch (error: any) {
-    next(ApiError.badRequest(error.message))
-  }
-}
+//       // Respond with the updated product
+//       // You might want to send a response to the client here
+//     } else {
+//       next(ApiError.unauthorized(`Product quantity for ${productId} is already zero.`))
+//     }
+//   } catch (error: any) {
+//     next(ApiError.badRequest(error.message))
+//   }
+// }
 
 export const updateOrder = (req: Request, res: Response, next: NextFunction) => {
   try {
