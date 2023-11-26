aaf35ea1fe6ede0e8222fd067a8b439f0bb8941a
Majedah <122641068+majowl77@users.noreply.github.com>
2023-11-26 09:48:19 +0300

    fix: users route naming

--- src/routers/users.ts
+++ src/routers/users.ts
@@ -12,14 +12,6 @@ router.get('/', getUsers)
 router.post('/register', registerNewUser)
 router.post('/login', loginUser)
 router.delete('/:userId', deleteUser)
-router.put('/:userId', updateUser)
-
-// i will work on it in the bouns and additional req
-// router.get('/:userId/page/:page', (req, res) => {
-//   res.json({
-//     msg: 'done',
-//     user: req.user,
-//   })
-// })
+router.put('profile/:userId', updateUser)
 
 export default router
