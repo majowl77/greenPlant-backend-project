e635a57b04ef3eb53f491c41a95a1e5a6e9270dd
Majedah <122641068+majowl77@users.noreply.github.com>
2023-12-26 21:52:25 +0300

    change some styling to the email

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -23,71 +23,71 @@ export async function sendActivationEmail(
 ) {
   const activationLink = `${dev.email.domain}/api/auth/activateUser/${activationToken}`
 
-  const htmlEmailTemplate = `  <section style="font-family:Raleway,sans-serif; display: flex; justify-content: center; align-items: center; flex-direction: column; max-width: 2xl; margin: auto; background-color: #ebebeb; padding: 126px 261px; background-image: url(https://i.ebayimg.com/images/g/2-4AAOSwTSpeVjmK/s-l1600.jpg);">
-  <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; background-color: #ffffff; padding: 35px 0px ;">
+  const htmlEmailTemplate = `  <section style="font-family:Raleway,sans-serif;display:flex;max-width:2xl;margin:auto;background-color:#ebebeb;padding:126px 261px; background-image: url(https://i.ebayimg.com/images/g/2-4AAOSwTSpeVjmK/s-l1600.jpg);">
+    <div style="display:flex;background-color:#ffffff;padding:35px 0px;justify-content: center;align-items: center;flex-direction: column;padding: 35px 51px;">
 
-  <header style="padding-bottom: 20px; border-radius: 20px ;">
-      <a href="#">
-          <img style="width: 400px; height: 90px; " src="https://majedah-bucket.s3.eu-west-2.amazonaws.com/greenPlantsWhiteLogo-1703597304054-405621210.png" alt="greenPlantLogo">
-      </a>
-  </header>
-  
-  <main style="margin-top: 8px;">
-      <h2 style="color: #335e33; font-size: 20px;">Hi ${firstName},</h2>
-  
-      <p style="font-size: 15px; margin-top: 2px; line-height: loose; color: #335e33; ">
-        Thank you for choosing GreenPlant!  <br>
-        Your account has been created. To activate your account and start shopping, 
-      <p style="font-size: 15px; color: #335e33;"> click the button below..
-      </p>
-      </p>
-      
-      <button style="  margin-top: 20px;  background-color: #FFFFFF;
-      border: 1px solid rgb(209,213,219);
-      border-radius: .5rem;
-      box-sizing: border-box;
-      color: #111827;
-      font-family: sans-serif;
-      font-size: .875rem;
-      font-weight: 600;
-      line-height: 1.25rem;
-      padding: .75rem 1rem;
-      text-align: center;
-      text-decoration: none #D1D5DB solid;
-      text-decoration-thickness: auto;
-      box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
-      cursor: pointer;
-      user-select: none;
-      -webkit-user-select: none;
-      touch-action: manipulation;">
-         <a href="${activationLink}" style="color: inherit; text-decoration: none;font-size: 15px;">  Activate your account 
-         </a> 
-      </button>
-      
+    <header style="padding-bottom: 20px; border-radius: 20px ;">
+        <a href="#">
+            <img style="width: 400px; height: 90px; " src="https://majedah-bucket.s3.eu-west-2.amazonaws.com/greenPlantsWhiteLogo-1703597304054-405621210.png" alt="greenPlantLogo">
+        </a>
+    </header>
+    
+    <main style="margin-top: 8px;">
+        <h2 style="color: #335e33; font-size: 20px;">Hi ${firstName},</h2>
+    
+        <p style="font-size: 15px; margin-top: 2px; line-height: loose; color: #335e33; ">
+          Thank you for choosing GreenPlant!  <br>
+          Your account has been created. To activate your account and start shopping, 
+        <p style="font-size: 15px; color: #335e33;"> click the button below..
+        </p>
+        </p>
+        
+        <button style="  margin-top: 20px;  background-color: #FFFFFF;
+        border: 1px solid rgb(209,213,219);
+        border-radius: .5rem;
+        box-sizing: border-box;
+        color: #111827;
+        font-family: sans-serif;
+        font-size: .875rem;
+        font-weight: 600;
+        line-height: 1.25rem;
+        padding: .75rem 1rem;
+        text-align: center;
+        text-decoration: none #D1D5DB solid;
+        text-decoration-thickness: auto;
+        box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+        cursor: pointer;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;">
+           <a href="${activationLink}" style="color: inherit; text-decoration: none;font-size: 15px;">  Activate your account 
+           </a> 
+        </button>
+        
 
-      <p style="font-size: 14px;margin-top: 49px; color: #335e33; ">
-      If you have any questions or need further assistance, please contact our <br>
-      support team at <a href="mailto:greenowls2023@gmail.com ">greenowls2023@gmail.com </a>
-      </p>
+        <p style="font-size: 14px;margin-top: 49px; color: #335e33; ">
+        If you have any questions or need further assistance, please contact our <br>
+        support team at <a href="mailto:greenowls2023@gmail.com ">greenowls2023@gmail.com </a>
+        </p>
 
-      <p style="font-size: 14px; margin-top: 30px; color: #335e33; ">
-       Thanks,<br>
-       GreenPlant Team
-      </p>
-  </main>
-  
-  
-  <footer style="margin-top: 8px; display: flex; justify-content: end; align-items: center; flex-direction: column; margin: 0px 160px; font-size: 11px; ">
-      <p style="color: #335e33; ">
-      This email was sent to <a href="#" style="color: blue-600; " target="_blank">${userEmail}</a>. 
-          If you'd rather not receive this kind of email, you can <a href="#" style="color: blue-600; ">unsubscribe</a> or <a href="#" style="color: blue-600; ">manage your email preferences</a>.
-      </p>
-  
-      <p style="margin-top: 30px; color: #335e33; ">© ${new Date().getFullYear()}  GreenPlant. All Rights Reserved.</p>
-  </footer>
-</div>
+        <p style="font-size: 14px; margin-top: 30px; color: #335e33; ">
+         Thanks,<br>
+         GreenPlant Team
+        </p>
+    </main>
+    
+    
+    <footer style="margin-top:8px;display:flex;margin: 0px -1px;font-size:11px;justify-content: center;align-items: center;flex-direction: column; ">
+        <p style="color: #335e33; ">
+        This email was sent to <a href="#" style="color: blue-600; " target="_blank">${userEmail}</a>. 
+            If you'd rather not receive this kind of email, you can <a href="#" style="color: blue-600; ">unsubscribe</a> or <a href="#" style="color: blue-600; ">manage your email preferences</a>.
+        </p>
+    
+        <p style="margin-top: 30px; color: #335e33; ">© ${new Date().getFullYear()}  GreenPlant. All Rights Reserved.</p>
+    </footer>
+  </div>
 
-  </section>
+    </section>
 `
 
   const mailOptions = {
